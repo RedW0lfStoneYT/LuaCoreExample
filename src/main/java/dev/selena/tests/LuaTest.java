c16185925259f820f70442d8eaef307f7325cc6e
Selena Rose Hammond <andregibby@gmail.com>
2023-08-27 12:17:09 +1200

    Initial commit

---
+++ src/main/java/dev/selena/tests/LuaTest.java
@@ -0,0 +1,2 @@
+package dev.selena.tests;public class LuaTest {
+}
---
+++ src/main/java/dev/selena/tests/MappingTest.java
@@ -0,0 +1,9 @@
+package dev.selena.tests;
+
+public class Mapped {
+
+    public String Test_String = "123";
+    public boolean Test_Boolean = false;
+    public int Test_Int = 1;
+
+}
